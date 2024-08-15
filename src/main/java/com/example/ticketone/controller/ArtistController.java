aef2474ff5e8874ca1097ac10bbfd1684a1b287c
hobisognodiunarma <alessandroarmari22@gmail.com>
2024-08-15 10:37:53 +0200

    first API

---
+++ src/main/java/com/example/ticketone/controller/ArtistController.java
@@ -0,0 +1,24 @@
+package com.example.ticketone.controller;
+
+import com.example.ticketone.entity.Artist;
+import com.example.ticketone.repository.ArtistRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("api/")
+@AllArgsConstructor
+public class ArtistController {
+
+    private final ArtistRepository artistRepository;
+
+    @GetMapping("get-all-artist")
+    public List<Artist> getAll(){
+        return artistRepository.findAll();
+    }
+}
--- src/main/java/com/example/ticketone/entity/typologic/Country.java
+++ src/main/java/com/example/ticketone/entity/typologic/Country.java
@@ -1,6 +1,7 @@
 package com.example.ticketone.entity.typologic;
 
 import com.example.ticketone.entity.Artist;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -20,5 +21,6 @@ public class Country {
     private String name;
 
     @OneToOne(mappedBy = "country")
+    @JsonIgnore
     private Artist artist;
 }
